430f3ae0ba3954bb33e177632835e9dafebca180
emag <lanabe.lanabe@gmail.com>
2017-03-10 19:54:34 +0900

    Add beans.xml for setting bean-discovery-mode

--- src/main/java/wildflyswarm/jpa_cdi_arq/DBConnectionController.java
+++ src/main/java/wildflyswarm/jpa_cdi_arq/DBConnectionController.java
@@ -1,13 +1,11 @@
 package wildflyswarm.jpa_cdi_arq;
 
 import javax.annotation.Resource;
-import javax.enterprise.context.ApplicationScoped;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
-@ApplicationScoped
 @Path("/")
 public class DBConnectionController {
 
